fcc950cf56fcca5e02ebac37db17db1b5c4b15d7
Louis301 <vlad.orlik@yandex.ru>
2023-03-29 14:45:12 +0300

    Chapter 4 was complete

--- django/polls_project/polls_project/urls.py
+++ django/polls_project/polls_project/urls.py
@@ -19,14 +19,12 @@ from django.urls import path
 from polls import views
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+app_name = 'polls'
 
-    path('polls/', views.index, name='index'),
-    
-    path('polls/<int:question_id>/', views.detail, name='detail'),  # ex: /polls/5/
-    
-    path('polls/<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
-    
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
